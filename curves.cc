d9540d5f37a0783a5c9ea66297e303a07a465bf3
Dmitrii Chich <86652230+mrdamos69@users.noreply.github.com>
2023-01-24 07:24:26 -0500

    Update curves.cc

--- curves.cc
+++ curves.cc
@@ -1,1 +1,39 @@
 #include "curves.h"
+
+std::pair<double, double> Circle::Point(double value) {
+  double x = radius_ * cos(value);
+  double y = radius_ * sin(value);
+  return std::make_pair(x, y);
+}
+
+std::pair<double, double> Circle::Derivative(double value) {
+  double dx = -radius_ * sin(value);
+  double dy = radius_ * cos(value);
+  return std::make_pair(dx, dy);
+}
+
+std::pair<double, double> Ellipse::Point(double value) {
+  double x = a_ * cos(value);
+  double y = b_ * sin(value);
+  return std::make_pair(x, y);
+}
+
+std::pair<double, double> Ellipse::Derivative(double value) {
+  double dx = -a_ * sin(value);
+  double dy = b_ * cos(value);
+  return std::make_pair(dx, dy);
+}
+
+std::pair<double, double> Helix::Point(double value) {
+  double x = a_ * cos(value);
+  double y = b_ * sin(value);
+  double z = c_ * cos(value);
+  return std::make_pair(x, y);
+}
+
+std::pair<double, double> Helix::Derivative(double value) {
+  double dx = -a_ * sin(value);
+  double dy = b_ * cos(value);
+  double dz = c_ * sin(value);
+  return std::make_pair(dx, dy);
+}
